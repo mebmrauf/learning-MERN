0ee38a1e1ede218c59d3989480cf35dda97fe46d
B M Rauf <bmrauf@outlook.com>
2024-10-25 23:28:53 +0600

    Update

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-    console.log(request);
-    // process.exit(0);
+    const url = request.url;
+    const method = request.method;
+
+    if (url === '/') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<head>');
+        response.write('<title>Hello World!</title>');
+        response.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Submit</button></form></body>');
+        response.write('</head>');
+        return response.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Ulala');
+        response.statusCode = 302;
+        response.setHeader('Location', '/');
+        return response.end();
+    }
     response.setHeader('Content-Type', 'text/html');
     response.write('<head>');
     response.write('<title>Hello World!</title>');
-    response.write('<body><h1>Good Evening everyone!</h1></body>');
+    response.write('<body><h1>Good Night everyone! Have a tight sleep</h1></body>');
     response.write('</head>');
     response.end();
 

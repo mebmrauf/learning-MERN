610fdace80bb1df4d25ecc91514dd1e7d9bc4b29
B M Rauf <bmrauf@outlook.com>
2024-10-27 19:33:49 +0600

    Update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+require("node:buffer");
 
 const server = http.createServer((request, response) => {
     const url = request.url;
@@ -8,25 +9,34 @@ const server = http.createServer((request, response) => {
     if (url === '/') {
         response.setHeader('Content-Type', 'text/html');
         response.write('<head>');
-        response.write('<title>Hello World!</title>');
-        response.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Submit</button></form></body>');
+        response.write('<title>NodeJS</title>');
+        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
         response.write('</head>');
         return response.end();
     }
 
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'Ulala');
-        response.statusCode = 302;
-        response.setHeader('Location', '/');
-        return response.end();
+        const body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            response.statusCode = 302;
+            response.setHeader('Location', '/');
+            return response.end();
+        });
+
     }
+
     response.setHeader('Content-Type', 'text/html');
     response.write('<head>');
     response.write('<title>Hello World!</title>');
     response.write('<body><h1>Good Night everyone! Have a tight sleep</h1></body>');
     response.write('</head>');
     response.end();
-
 });
 
 server.listen(3000);

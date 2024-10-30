c10c0f7dac7f6b04f91699fac934969e21fe42ec
B M Rauf <bmrauf@outlook.com>
2024-10-30 13:01:20 +0600

    update

--- README.txt
+++ README.txt
@@ -3,3 +3,7 @@ for installing npm run this command in terminal
 1. npm install --save-dev (developer dependency)
 2. npm install (production dependency)
 3. npm install -g (install in the device)
+
+install express js
+
+npm install --save express
--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line
+});
 
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send("<h1>Hello fromm Express!</h1>")
+});
+
+const server = http.createServer(app);
 server.listen(3000);
